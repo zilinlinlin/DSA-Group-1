d96cbce1538d9b3c931c91c369885715ee20135c
Wyatt <wyatthmip@gmail.com>
2026-03-06 15:24:44 +0000

    first commit, old data

---
+++ plot_data.m
@@ -0,0 +1,19 @@
+%% load variables
+load("data\long log.mat")
+
+%% plot map
+geoplot(Position.latitude, Position.longitude);
+geobasemap streets
+
+%% load audio
+clc
+
+[signal, Fs] = audioread('data\long log.m4a');
+t = (0:length(signal)-1) / Fs;
+
+%% plot audio
+stem(t, signal(:,1), 'filled', 'MarkerFaceColor', 'red');
+xlabel('Time (seconds)');
+ylabel('Amplitude');
+title('Stem Plot of Audio Signal');
+grid on;
