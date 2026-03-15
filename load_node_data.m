6cb7f7acc57acf649d9d31bc8f1a225c66c7aa16
Wyatt <wyatthmip@gmail.com>
2026-03-15 15:33:33 +0000

    updated node points

--- load_node_data.m
+++ load_node_data.m
@@ -1,14 +1,69 @@
-Name = ["Marshgate";"One Pool Street";"Itsu";"Waitrose";"McDonalds"; ...
-        "Caffe Nero";"Greggs";"P1";"P2";"P3";"P4";"P5";"P6";"P7";"P8";"P9";"P10"];
+% Names
+names = {
+'Marshgate'
+'One Pool Street'
+'Itsu'
+'Waitrose'
+'McDonalds'
+'Caffe Nero'
+'Greggs'
+'P1'
+'P2'
+'P3'
+'P4'
+'P5'
+'P6'
+'P7'
+'P8'
+'P9'
+'P10'
+};
 
-Lat = [51.53780;51.53870;51.54190;51.54380;51.54360;51.54280;51.54300; ...
-       51.53820;51.54050;51.54080;51.53860;51.54200;51.54090;51.54270; ...
-       51.54390;51.54270;51.54330];
+% Data: [Latitude Longitude Type]
+data = [
+51.53780  -0.01155  1
+51.53870  -0.00972  1
+51.54190  -0.00920  1
+51.54380  -0.00876  1
+51.54360  -0.00596  1
+51.54280  -0.00544  1
+51.54300  -0.00490  1
+51.53820  -0.01152  0
+51.54060  -0.01188  0
+51.54080  -0.01082  0
+51.53850  -0.00855  0
+51.54210  -0.00875  0
+51.54090  -0.00676  0
+51.54270  -0.00756  0
+51.54390  -0.00667  0
+51.54270  -0.00463  0
+51.54330  -0.00537  0
+];
 
-Lon = [-0.01155;-0.00972;-0.00920;-0.00876;-0.00596;-0.00544;-0.00490; ...
-       -0.01152;-0.01166;-0.01072;-0.00837;-0.00840;-0.00676;-0.00756; ...
-       -0.00667;-0.00463;-0.00537];
+lat = data(:,1);
+lon = data(:,2);
+type = data(:,3);
 
-Type = [1;1;1;1;1;1;1;0;0;0;0;0;0;0;0;0;0];
+% Separate the two groups
+lat1 = lat(type==1);
+lon1 = lon(type==1);
 
-locations = table(Name,Lat,Lon,Type);
+lat0 = lat(type==0);
+lon0 = lon(type==0);
+
+% Create geographic plot
+figure
+geoscatter(lat1,lon1,80,'red','filled')
+hold on
+geoscatter(lat0,lon0,80,'blue','filled')
+
+% Add labels
+for i = 1:length(names)
+    text(lat(i),lon(i),names{i},'FontSize',8)
+end
+
+% Map style
+geobasemap streets
+
+title('Location Map')
+legend('Restaurants','Candidate Points')
