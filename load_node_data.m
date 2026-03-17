23eb6dffb6569f4470fb6bb2c73a48e83ad52f47
zilinlinlin <zilinpang7@gmail.com>
2026-03-17 13:35:36 +0000

    Add files via upload
    load_node_data + distances and graph for q 1 2 3 7 8

--- load_node_data.m
+++ load_node_data.m
@@ -1,6 +1,23 @@
 % Names
-names = ["Marshgate";"One Pool Street";"Itsu";"Waitrose";"McDonalds"; ...
-        "Caffe Nero";"Greggs";"P1";"P2";"P3";"P4";"P5";"P6";"P7";"P8";"P9";"P10"];
+names = {
+'Marshgate'
+'One Pool Street'
+'Itsu'
+'Waitrose'
+'McDonalds'
+'Caffe Nero'
+'Greggs'
+'P1'
+'P2'
+'P3'
+'P4'
+'P5'
+'P6'
+'P7'
+'P8'
+'P9'
+'P10'
+};
 
 % Data: [Latitude Longitude Type]
 data = [
@@ -44,9 +61,62 @@ geoscatter(lat0,lon0,80,'blue','filled')
 for i = 1:length(names)
     text(lat(i),lon(i),names{i},'FontSize',8)
 end
-
+distances = [];
+function dist = plot_distance(point1, point2)
+hold on
+    lat1 = point1(1);
+    lon1 = point1(2);
+    lat2 = point2(1);
+    lon2 = point2(2);
+    arclen = distance(lat1,lon1,lat2,lon2);
+    dist = deg2km(arclen)*1000; %distance in metres
+    midLat = (lat1+lat2)/2;
+    midLon = (lon1+lon2)/2;
+    
+    geoplot([lat1 lat2], [lon1 lon2], 'k-');
+    text(midLat,midLon,sprintf('%f m',dist),'FontSize',7, 'FontWeight','normal', 'Color', 'black');
+end
 % Map style
 geobasemap streets
-
 title('Location Map')
 legend('Main Points','Passing Points')
+%correspond points to data
+marshgate = [data(1,1), data(1,2)];
+ops = [data(2,1), data(2,2)];
+itsu = [data(3,1), data(3,2)];
+waitrose = [data(4,1), data(4,2)];
+mcdonalds = [data(5,1), data(5,2)];
+nero = [data(6,1), data(6,2)];
+greggs = [data(7,1), data(7,2)];
+p1 = [data(8,1), data(8,2)];
+p2 = [data(9,1), data(9,2)];
+p3 = [data(10,1), data(10,2)];
+p4 = [data(11,1), data(11,2)];
+p5 = [data(12,1), data(12,2)];
+p6 = [data(13,1), data(13,2)];
+p7 = [data(14,1), data(14,2)];
+p8 = [data(15,1), data(15,2)];
+p9 = [data(16,1), data(16,2)];
+p10 = [data(17,1), data(17,2)];
+distance = [plot_distance(marshgate, p1);
+    plot_distance(p1, ops);
+    plot_distance(ops, p2);
+    plot_distance(ops, p3);
+    plot_distance(ops, p4);
+    plot_distance(p2, itsu);
+    plot_distance(p3, itsu);
+    plot_distance(itsu, p5);
+    plot_distance(p5, p6);
+    plot_distance(p5, p7)
+    plot_distance(p4, p6);
+    plot_distance(p6, p9);
+    plot_distance(p7, nero);
+    plot_distance(p7, mcdonalds);
+    plot_distance(p7, waitrose);
+    plot_distance(nero, p9);
+    plot_distance(p9, greggs);
+    plot_distance(greggs, p10);
+    plot_distance(waitrose, p8);
+    plot_distance(p8, mcdonalds);
+    plot_distance(mcdonalds, p10)];
+disp(distance); %distance in metres
